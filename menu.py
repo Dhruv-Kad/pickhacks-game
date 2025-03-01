c6c306a2a312068451ba42070c43782509fb9ed5
Jonzapolski456 <jonathanzapolski@gmail.com>
2025-02-28 21:45:22 -0600

    menu

--- menu.py
+++ menu.py
@@ -1,14 +1,21 @@
 
+
+
+
 def menu():
-    print ("-----------------------------------------------------------")
+    print ("-----------------------------|-----------------------------")
     print ("                     Non-Proliferation                     ")
-    print ("-----------------------------------------------------------")
-    print ("                         1 Player                          ")
-    print ("                         2 Player                          ")
-    print ("                           Exit                            ")
+    print ("-----------------------------|-----------------------------")
+    print ("                        1. 1 Player                        ")
+    print ("                        2. 2 Player                        ")
+    print ("                        3.   Exit                          ")
+    choice = input("Please select an option: ")
+    if (choice == 1):
+        setup1()
 
 
-
+def setup1():
+    print ("Please select the coordinates for your character")
 
 
 menu()
