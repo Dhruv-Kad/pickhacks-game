786eba7d2b8620d74fd2774653ba5da012bcbb83
dhruv kadam <dhruv@minitron.my.domain>
2025-03-01 23:11:34 -0600

    Fixed menu, added breifing

--- briefing.py
+++ briefing.py
@@ -1,4 +1,5 @@
 def brief():
     print("""Hello commander. As you may have known, tensions with our neighbour, the Bloviet Munion 
     have been approaching a boiling point. Our spies have been reporting rapid troop movments
-    and nuclear a""")
+    and nuclear silo deployments in the Munion and their satalite states. Your job is to stop the 
+    spread of their influnce by any means nesscarcy.""")
