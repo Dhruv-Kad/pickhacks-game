2c2656cb105aadbca73cef7159d77a8456d8051a
Jonzapolski456 <jonathanzapolski@gmail.com>
2025-03-02 09:14:47 -0600

    thing

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def returnmoney():
     return p1_money
 def shop():
     print(f"---SHOP---")
-    print(f"1. nuke    | ${nuke_price}")
+    print(f"1. nuke    | ${nuke_price} [Destroys all land in a 2 unit radius form a selected point]")
     print(f"2. spy     | ${p1_spy_price} [Increases percent chance to reveal an enemy base each round by 2%]")
     print(f"3. factory | ${p1_factory_price} [Increase money earned every round $250] (You can only build a factory ten units away from a base)")
     print(f"4. coup    | ${coup_base_price} / {round(coup_base_price * 1.5)} / {coup_base_price * 2} [Takes over a square, risking conflict if there is an enemy base within a radius. More expensive coups lower this radius")
