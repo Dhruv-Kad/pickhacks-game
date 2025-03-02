6ea41159a909bc7bdfd475d26bf2a4790531dbe7
dhruv kadam <dhruv@minitron.my.domain>
2025-03-01 18:41:07 -0600

    Final touches to enemy AI.

--- enemyai.py
+++ enemyai.py
@@ -5,11 +5,18 @@ def enemyturn(playerfactories,playerbunkers,enemybunkersdestroyed,difficulty,ten
     spy_weight = 1+((enemybunkersdestroyed * difficulty) * (playerbunkers * 0.4) * (abs(tension-9)+1)*(money/3))
     factory_weight = 1+((enemybunkersdestroyed * difficulty) * (enemybunkersdestroyed) * (abs(tension-10)+1)*(money/2))
     print(f"nuke_weight {nuke_weight} spy_weight {spy_weight} factory_weight {factory_weight}")
-    
+    int x = 0
     #possible_actions = ['nuke/coup','spy','factory']
+    prices = [nuke_price, spy_price, factory_price]
     possible_actions = [1,2,3]
+    cpossible_actions= {1 : (nuke_price), 2 : (spy_price), 3 : (factory_price), 4: 0} 
     weights = [nuke_weight, spy_weight, factory_weight]
     selected_action = random.choices(possible_actions, weights=weights, k=1)[0]
+    if money < prices.index(selected_action):
+        for x in cpossible_actions:
+            selmoney = cpossible_actions[x]
+            if(money > selmoney):
+                return(x)
     return [selected_action,priceofselectedaction]
 #input weight for testing the weights
 def weighttester():
